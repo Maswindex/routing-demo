2f30bf22c58e5a792d8cfa89f9c5c638a6aaf5a8
Mason Hernandez <mhernandez58@mail.greenriver.edu>
2018-01-12 17:09:30 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+require_once("vendor/autoload.php");
+
+//create an instance of the Base class
+$f3 = Base::instance();
+
+//Define a default route
+$f3->route('GET /', function () {
+    $view = new View;
+    echo $view->render
+    ('views/home.php');
+});
+
+//run fat free
+$f3->run();
