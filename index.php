8852839e6ecae8b78daee52e461b9372ddd4776f
Mason Hernandez <mhernandez58@mail.greenriver.edu>
2018-01-16 16:20:17 -0500

    Made a toe ring page

--- index.php
+++ index.php
@@ -17,5 +17,11 @@ $f3->route('GET /page1', function () {
     echo '<h2>Page 1<h2>';
 });
 
+//Define a page1 route
+$f3->route('GET /jewelry/rings/toe-rings', function () {
+    $template = new Template();
+    echo $template->render('views/toe-rings.html');
+});
+
 //run fat free
 $f3->run();
